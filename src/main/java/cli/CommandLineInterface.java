addf863a7b79d043032fd181bb92828da948b3c6
AndroidTheHuman <24109320@student.hhs.nl>
2025-04-12 15:16:39 +0200

    n

--- src/main/java/cli/CommandLineInterface.java
+++ src/main/java/cli/CommandLineInterface.java
@@ -3,4 +3,23 @@ import java.util.Scanner;
 
 public class CommandLineInterface {
     Scanner scanner = new Scanner(System.in);
+    public void startUp() {
+        System.out.println("Hallo!");
+        mainMenu();
+    }
+    private void mainMenu() {
+        while(true) {
+            System.out.print(">");
+            String input = scanner.nextLine();
+
+        }
+    }
+    private void handleCommand(String input) {
+        switch(input.toLowerCase()) {
+            case "hallo":
+                System.out.println("Hallo 2!");
+                break;
+        }
+    }
+
 }
